23d58317624f822d87ba546d2556b9a8c0942b35
Vocho <vocho@underdog.gs>
2016-07-21 11:20:10 -0500

    terminado
    menu flotante
    correction en webkit para que se sobre ponga la imagen

--- js/jquery.uniformes.js
+++ js/jquery.uniformes.js
@@ -19,6 +19,52 @@
 					$(window).resize(function(){
 						componentObj.methods.resize();
 					});
+					componentObj.methods.menu();
+				},
+				changeImage: function(){
+					var clubes = ["Atlas", "Chivas", "Mty", "Qro", "Santos"];
+					var home = "home";
+					var away = "away";
+					var cambio = false;
+					setInterval(function(){
+						$(".landing-thumbs").each(function(i, val){
+						var imagen1 = $(this).find("img").get(0);
+						var imagen2 = $(this).find("img").get(1);
+						var imagen3 = $(this).find("img").get(2);
+						var imagen4 = $(this).find("img").get(3);
+						$(imagen1).attr("src",urlIndepth+"images/"+clubes[i].toLowerCase()+"/detalles/"+clubes[i]+"_"+((cambio)?home:away)+"1.jpg");
+						$(imagen2).attr("src",urlIndepth+"images/"+clubes[i].toLowerCase()+"/detalles/"+clubes[i]+"_"+((cambio)?home:away)+"2.jpg");
+						$(imagen3).attr("src",urlIndepth+"images/"+clubes[i].toLowerCase()+"/detalles/"+clubes[i]+"_"+((cambio)?home:away)+"3.jpg");
+						$(imagen4).attr("src",urlIndepth+"images/"+clubes[i].toLowerCase()+"/detalles/"+clubes[i]+"_"+((cambio)?home:away)+"4.jpg");
+					});	
+					cambio = (cambio)?false:true;
+					}, 5000);
+				},
+				menu: function(){
+					$('#atlas').waypoint(componentObj.methods.menuChange);
+					$('#chivas').waypoint(componentObj.methods.menuChange);
+					$('#mty').waypoint(componentObj.methods.menuChange);
+					$('#qro').waypoint(componentObj.methods.menuChange);
+					$('#santos').waypoint(componentObj.methods.menuChange);
+					$("#comments").waypoint(function(direction){
+						if(direction === "down"){
+							$("#puma-menu").fadeOut("fast");
+						}else{
+							$("#puma-menu").fadeIn("fast");
+						}
+					});
+				},
+				menuChange: function(direction){
+					var menuName = "#"+$(this).data("menu");
+					if(menuName == "#atlas-menu"){
+						if(direction == "down"){
+							$("#puma-menu").fadeIn("fast");
+						}else{
+							$("#puma-menu").fadeOut("slow");
+						}
+					}
+					$(".equipos-menu").removeClass("active");
+					$(menuName).addClass("active");
 				},
 				resize: function(){
 					if($(window).width() > 768){
@@ -58,28 +104,8 @@
 						"background-color":"transparent"
 					});
 					$(img).attr("src",urlIndepth+"images/portada/uniformes/"+nombre+"_home.png");
-				},
-				changeImage: function(){
-					var clubes = ["Atlas", "Chivas", "Mty", "Qro", "Santos"];
-					var home = "home";
-					var away = "away";
-					var cambio = false;
-					//while(true){
-						setInterval(function(){
-							$(".landing-thumbs").each(function(i, val){
-							var imagen1 = $(this).find("img").get(0);
-							var imagen2 = $(this).find("img").get(1);
-							var imagen3 = $(this).find("img").get(2);
-							var imagen4 = $(this).find("img").get(3);
-							$(imagen1).attr("src",urlIndepth+"images/"+clubes[i].toLowerCase()+"/detalles/"+clubes[i]+"_"+((cambio)?home:away)+"1.jpg");
-							$(imagen2).attr("src",urlIndepth+"images/"+clubes[i].toLowerCase()+"/detalles/"+clubes[i]+"_"+((cambio)?home:away)+"2.jpg");
-							$(imagen3).attr("src",urlIndepth+"images/"+clubes[i].toLowerCase()+"/detalles/"+clubes[i]+"_"+((cambio)?home:away)+"3.jpg");
-							$(imagen4).attr("src",urlIndepth+"images/"+clubes[i].toLowerCase()+"/detalles/"+clubes[i]+"_"+((cambio)?home:away)+"4.jpg");
-						});	
-						cambio = (cambio)?false:true;
-						}, 5000);
-					//}		
 				}
+				
 			}
 		};
 		return componentObj;
